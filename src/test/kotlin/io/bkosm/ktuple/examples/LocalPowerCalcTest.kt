03057e35738d73f724e10dc7eaee2a1f36d6ad94
bkosm <bartoshmen@gmail.com>
2022-11-16 18:14:12 +0100

    add pseudo cool example

---
+++ src/test/kotlin/io/bkosm/ktuple/examples/LocalPowerCalcTest.kt
@@ -0,0 +1,32 @@
+package io.bkosm.ktuple.examples
+
+import io.bkosm.ktuple.examples.LocalPowerCalcTest.PowerCalc
+import io.bkosm.ktuple.t
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+import kotlin.math.pow
+import kotlin.test.expect
+
+internal class LocalPowerCalcTest {
+
+    fun interface PowerCalc : (Number, Double) -> Double {
+        companion object
+    }
+
+    private val PowerCalc.Companion.inMemory
+        get() = PowerCalc { a, b -> a.toDouble().pow(b) }
+
+    @TestFactory
+    fun `it works!`() = listOf(
+        t(1, 2, 1.0, "with exponent of 2"),
+        t(2, 2, 4.0, "with base of 2"),
+        t(3, 3, 27.0, "with exponents higher than 2"),
+        t(2.2, 3.3, 13.489468760533386, "with doubles")
+    ).map { (a, b, result, name) ->
+        dynamicTest(name) {
+            val uut = PowerCalc.inMemory
+            expect(result) { uut(a, b.toDouble()) }
+        }
+    }
+
+}
